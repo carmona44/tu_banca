8f3249e972ed7bd1d600a85b99f89c1b3b33e974
Daniel <dam2015.grupo3@gmail.com>
2017-08-09 19:28:01 +0200

    Listado de movimientos

--- javascript/app.js
+++ javascript/app.js
@@ -2,11 +2,12 @@
  * Created by Daniel on 08/08/2017.
  */
 
-angular.module('tu_banca', ['ui.router'])
-    .config(function ($stateProvider, $urlRouterProvider) {
+angular.module('tuBanca', ['ui.router', 'ngMaterial'])
+    .config(function ($stateProvider, $urlRouterProvider, $mdDateLocaleProvider) {
         $stateProvider.state({
             name: 'cartera',
             url: '/cartera',
+            controller: 'CarteraController',
             templateUrl: 'templates/cartera.html'
         })
             .state({
@@ -16,4 +17,134 @@ angular.module('tu_banca', ['ui.router'])
             });
 
         $urlRouterProvider.otherwise('/cartera');
-});
+        $mdDateLocaleProvider.firstDayOfWeek = 1;
+})
+    .controller('CarteraController', carteraController);
+
+function carteraController($scope) {
+    $scope.movimientos = [
+        {
+            cantidad: 1234,
+            fecha: '25/05/2016',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 12894,
+            fecha: '28/05/2016',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'gasto'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'gasto'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'gasto'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'gasto'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'gasto'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'gasto'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'gasto'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'gasto'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'ingreso'
+        },
+        {
+            cantidad: 648,
+            fecha: '25/08/2017',
+            tipo: 'gasto'
+        }
+    ];
+    $scope.ingresos = 0;
+    $scope.gastos = 0;
+    $scope.total = 0;
+
+    for (var i=0; i<$scope.movimientos.length; i++){
+        if($scope.movimientos[i].tipo == 'gasto'){
+            $scope.gastos += $scope.movimientos[i].cantidad;
+        } else {
+            $scope.ingresos += $scope.movimientos[i].cantidad;
+        }
+    }
+
+    $scope.total = $scope.ingresos - $scope.gastos;
+}
