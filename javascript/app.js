8d5a207581e57d599633e4322348b6590f8dfd10
carmona44 <daniel_95ca@hotmail.com>
2017-08-11 13:18:06 +0200

    Añadida descripción movimiento y probando gráfica

--- javascript/app.js
+++ javascript/app.js
@@ -28,7 +28,8 @@ function carteraController($scope) {
         {
             cantidad: 1234,
             fecha: '25/05/2016',
-            tipo: 'ingreso'
+            tipo: 'ingreso',
+            descripcion: 'Nomina mensual porque yo lo valgofdsssssssssssssss ssssssssdfsfaaaa'
         },
         {
             cantidad: 12894,
@@ -163,18 +164,13 @@ function estadisticasController() {
                 x: -20
             },
             xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                categories: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+                    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
             },
             yAxis: {
                 title: {
                     text: 'Cantidad (€)'
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
+                }
             },
             tooltip: {
                 valueSuffix: '€'
@@ -187,13 +183,16 @@ function estadisticasController() {
             },
             series: [{
                 name: 'Ingresos',
-                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+                data: [7.0, 6.9, 9.5, 12.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+                color: '#351BEF'
             }, {
                 name: 'Total',
-                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5],
+                color: '#000000'
             }, {
                 name: 'Gastos',
-                data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+                data: [0, 0, 0, 0, 0, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0],
+                color: '#FF0000'
             }]
         });
     });
